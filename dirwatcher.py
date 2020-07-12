efd88e77fb3082c503d06f6543b8962ab0d84747
Kevin Blount <kvbchamp@gmail.com>
2020-07-11 23:42:02 -0400

    setup logging formatter for console handler

--- dirwatcher.py
+++ dirwatcher.py
@@ -62,6 +62,13 @@ author = "Kevin Blount"
 
 exit_flag = False
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter(
+    '%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
 
 
 def signal_handler(sig_num, frame):
